c691a00daef74e5ae84e90ff0e51fc81aab192c9
robin <zerolling619>
2025-02-11 12:15:05 -0500

    act

---
+++ js/pelicula.js
@@ -0,0 +1,47 @@
+const getPelicula = async ()=>{
+    const id = new URLSearchParams(window.location.search).get('id')
+    const data = await fetch(`https://localhost/cinestar_sweb/peliculas/${id}`)
+    if(data.status == 200){
+        let pelicula = await data.json()
+        pelicula = pelicula.data
+        let html = `
+            <br/><h1>Cartelera</h1><br/>
+				<div class="contenido-pelicula">
+					<div class="datos-pelicula">
+						<h2>${pelicula.Titulo}</h2>
+						<p>${pelicula.Sinopsis}</p>
+						<br/>
+						<div class="tabla">
+							<div class="fila">
+								<div class="celda-titulo">Título Original :</div>
+								<div class="celda">${pelicula.Titulo}(Todo Público)</div>
+							</div>
+							<div class="fila">
+								<div class="celda-titulo">Estreno :</div>
+								<div class="celda">${pelicula.FechaEstrenoss}</div>
+							</div>
+							<div class="fila">
+								<div class="celda-titulo">Género :</div>
+								<div class="celda">${pelicula.Geneross}</div>
+							</div>
+							<div class="fila">
+								<div class="celda-titulo">Director :</div>
+								<div class="celda">${pelicula.Director}</div>
+							</div>
+							<div class="fila">
+								<div class="celda-titulo">Reparto :</div>
+								<div class="celda">${pelicula.Reparto}</div>
+							</div>
+						</div>
+					</div>
+					<img src="img/pelicula/1.jpg" width="160" height="226"><br/><br/>
+				</div>
+				<div class="pelicula-video">
+					<embed src="https://www.youtube.com/v/6maujJFcuxAfs" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="580" height="400">
+				</div>`
+
+    }
+    document.getElementById('contenido-interno').innerHTML = html
+}
+
+getPelicula()
